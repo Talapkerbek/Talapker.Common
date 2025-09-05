5c06f07e3fd04f852e3a278f1854a0c985f71efe
Islambek Gazizov <islambekgazizovv@gmail.com>
2025-09-05 22:55:30 +0500

    I fixed warnings

--- src/Settings/EFCoreSettings.cs
+++ src/Settings/EFCoreSettings.cs
@@ -2,11 +2,11 @@
 
 public class EFCoreSettings
 {
-    public string Host { get; init; }
+    public string Host { get; init; } = String.Empty;
     public int Port { get; init; }
-    public string UserName { get; init; }
-    public string Password { get; init; }
-    public string Database { get; init; }
+    public string UserName { get; init; } = String.Empty;
+    public string Password { get; init; } = String.Empty;
+    public string Database { get; init; } = String.Empty;
 
     public string ConnectionString => $"Host={Host};Port={Port};Username={UserName};Password={Password};Database={Database}";
 }
